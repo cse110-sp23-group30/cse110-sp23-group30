74a973cf662eba3efed240896eca54b9310c78f7
Eric Dong <e1dong@ucsd.edu>
2023-05-21 14:04:19 -0700

    basic html and js for the cart page
    No CSS yet, but I'm not really sure what the entire art style should be like.
    Also the js doesn't completely work as I'm not sure how to coordinate an array of items with other screens yet

---
+++ source/cart.js
@@ -0,0 +1,23 @@
+function addPlate() {
+  location.href = "plate.html";
+}
+
+function addBowl() {
+  location.href = "bowl.html";
+}
+
+function purchase() {
+  location.href = "confirm.html";
+}
+
+function popUp() {
+  const confirmClear = document.getElementById("confirm-clear");
+  confirmClear.style.display =
+    confirmClear.style.display === "none" ? "block" : "none";
+}
+
+function confirmClear() {
+  const items = document.getElementById("items");
+  items.innerHTML = "";
+  popUp();
+}
