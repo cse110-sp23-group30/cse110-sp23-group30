8cb6f1e812613db57236f262c0538972c7cc15ad
Ryan Wong <r3wong@ucsd.edu>
2023-05-30 13:07:46 -0700

    Welcome/Home Screen - Sprint 2 Fixes #61

--- source/opening_screen/opening-screen.js
+++ source/opening_screen/opening-screen.js
@@ -1,7 +1,6 @@
 window.addEventListener('DOMContentLoaded', init);
 
 function init() {
-    document.getElementById("settings-button").addEventListener("click", showSettings);
     document.getElementsByClassName("close")[0].addEventListener("click", closeSettings);
     document.getElementById("instructions-button").addEventListener("click", openInstructions);
     document.getElementById("play-button").addEventListener("click", playGame);
@@ -13,12 +12,4 @@ function openInstructions() {
 
 function playGame() {
     // Link to Food Selection Screen
-}
-
-function showSettings() {
-    document.getElementById("settings-popup").style.display = "block";
-}
-
-function closeSettings() {
-    document.getElementById("settings-popup").style.display = "none";
 }
