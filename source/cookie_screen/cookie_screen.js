9fbd135510a6e934fe9e72e0455e9e838c75349d
Jackson-Wang-0 <zhw049@ucsd.edu>
2023-06-03 02:03:21 -0700

    Sprint 3

--- source/cookie_screen/cookie_screen.js
+++ source/cookie_screen/cookie_screen.js
@@ -1,8 +1,29 @@
-function playBackgroundMusic() {
-    const backgroundMusic = document.getElementById('background-music');
-    backgroundMusic.play();
-}
+// const variables
+const cookie = document.getElementById("fortune-cookie");
+const message = document.getElementById("fortune-message");
+const backgroundMusic = document.getElementById('background-music');
+const buttonsContainer = document.querySelector(".buttons_container");
 
+// videos 
+var videoPlayer = document.getElementById('video-player');
+var fullscreenVideo = document.getElementById('fullscreen-video');
+var videoStarted = false;
+
+// volumes
+const volumeSlider = document.getElementById("volume-slider");
+const volumeIcon = document.getElementById("volume-icon");
+
+// credit
+const creditElement = document.getElementById("credit");
+
+// video array for animation
+const videos = [
+    "media/video_blue.mp4",
+    "media/video_gold.mp4",
+    "media/video_purple.mp4"
+];
+
+// array of messages
 const messages = [
     /*        message length for better display         */
     "You will have a prosperous future.",
@@ -28,91 +49,116 @@ const messages = [
     "You are destined for greatness.",
     "Your kindness will be rewarded tenfold.",
 ];
-  
+
+
 function shakeCookie() {
-    const cookie = document.getElementById("fortune-cookie");
-    const message = document.getElementById("fortune-message");
-
-    cookie.innerHTML = '<img src="cookie_before.png" alt="Cracked Cookie">';
-    cookie.classList.add("shake-animation");
-
+    // prevent user clicking multiple times
+    cookie.classList.remove("shake-animation");
     // Clear the fortune message
     message.textContent = "";
     // Hide the fortune message
     message.style.display = "none";
 
-    setTimeout(openFortune, 1000); // Wait 1 second for the shake animation to complete
+    cookie.innerHTML = '<img src="media/cookie_before.png" alt="Cracked Cookie">';
+    // Clear the fortune message
+    message.textContent = "";
+    // Hide the fortune message
+    message.style.display = "none";
+
+    // Increase the credit by 100
+    let credit = parseInt(creditElement.textContent);
+    credit += 100;
+    creditElement.textContent = credit;
+
+    // Randomly select a video from the 'videos' array
+    const randomIndex = Math.floor(Math.random() * videos.length);
+    const randomVideo = videos[randomIndex];
+
+    // Set the source of the video player to the randomly selected video
+    videoPlayer.src = randomVideo;
+    fullscreenVideo.style.display = 'block';
+    videoPlayer.play();
+
+    // skip if dont wanna see animation
+    document.addEventListener("click", skipVideo);
+
 }
-  
+
+function skipVideo() {
+    if (videoStarted && videoPlayer.currentTime != videoPlayer.duration) {
+        videoPlayer.currentTime = videoPlayer.duration;
+    }
+}
+if (videoPlayer) {
+    videoPlayer.addEventListener('playing', function() {
+        videoStarted = true;
+    });
+
+    videoPlayer.addEventListener('ended', function() {
+        fullscreenVideo.style.display = 'none';
+        openFortune();
+    });
+}
+
 function openFortune() {
-    const cookie = document.getElementById("fortune-cookie");
+    // remove animation before another if user clicks frequenly
     cookie.classList.remove("shake-animation");
-    const message = document.getElementById("fortune-message");
-  
-    // Replace the image source with the cracked cookie image
-    cookie.innerHTML = '<img src="cookie_after.png" alt="Cracked Cookie">';
-  
-    const randomIndex = Math.floor(Math.random() * messages.length);
-    message.textContent = messages[randomIndex];
-    message.style.display = "block";
+    // Clear the fortune message
+    message.textContent = "";
+    // Hide the fortune message
+    message.style.display = "none";
+
+    cookie.classList.add("shake-animation");
+
+    setTimeout(function() {
+        cookie.classList.remove("shake-animation");
+    
+        // Replace the image source with the cracked cookie image
+        cookie.innerHTML = '<img src="media/cookie_after.png" alt="Cracked Cookie">';
+        
+        const randomIndex = Math.floor(Math.random() * messages.length);
+        message.textContent = messages[randomIndex];
+        message.style.display = "block";
+    }, 1000);
 }
 
 function restartGame() {
-    // Reset the fortune cookie and message
-    const cookie = document.getElementById("fortune-cookie");
-    const message = document.getElementById("fortune-message");
-  
     // Replace the image source with the initial cookie image
-    cookie.innerHTML = '<img src="cookie_before.png" alt="Fortune Cookie">';
-  
+    cookie.innerHTML = '<img src="media/cookie_before.png" alt="Fortune Cookie">';
+
     // Clear the fortune message
     message.textContent = "";
     // Hide the fortune message
     message.style.display = "none";
-
     // go to cart screen
-
+    window.location.href = "../cart_screen/cart.html";
 }
-  
-function goToHome() {
-    // Reset the fortune cookie and message
-    const cookie = document.getElementById("fortune-cookie");
-    const message = document.getElementById("fortune-message");
 
+function goToHome() {
     // Replace the image source with the initial cookie image
-    cookie.innerHTML = '<img src="cookie_before.png" alt="Fortune Cookie">';
+    cookie.innerHTML = '<img src="media/cookie_before.png" alt="Fortune Cookie">';
 
     // Clear the fortune message
     message.textContent = "";
     // Hide the fortune message
     message.style.display = "none";
-
     // go to home 
-
-   
+    window.location.href = "../opening_screen/opening-screen.html";
 }
+if (volumeSlider) {
+    volumeSlider.addEventListener("input", () => {
+        const volumeLevel = volumeSlider.value / 100;
+        backgroundMusic.volume = volumeLevel;
+        backgroundMusic.play();
 
-// cookie background animation
-function generateFloatingCookies() {
-    const numCookies = 8; // Number of floating cookies to generate
-  
-    for (let i = 0; i < numCookies; i++) {
-      const delay = Math.random() * 10; // Random delay in seconds
-      
-      setTimeout(() => {
-        const floatingCookie = document.createElement("div");
-        floatingCookie.classList.add("floating-cookie");
-        floatingCookie.style.setProperty("--x", Math.random().toFixed(2));
-        
-        const cookieImage = document.createElement("img");
-        cookieImage.src = "fortune-cookie.png";
-        floatingCookie.appendChild(cookieImage);
-        
-        document.body.appendChild(floatingCookie);
-      }, delay * 1250); // Convert delay to milliseconds
-    }
+        if (volumeLevel < 0.5) {
+          volumeIcon.src = "media/volume-level-1.svg";
+          volumeIcon.alt = "Volume level 1";
+        } else {
+          volumeIcon.src = "media/volume-level-2.svg";
+          volumeIcon.alt = "Volume level 2";
+        }
+});
 }
-generateFloatingCookies();
 
 module.exports = {shakeCookie, openFortune, restartGame, goToHome}
-
