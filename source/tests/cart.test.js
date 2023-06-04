45d57c198ec49ea3ec336425874ee36b3335c594
Min <jaeminko0420@gmail.com>
2023-06-04 16:31:05 -0700

    Update cart.test.js

--- source/tests/cart.test.js
+++ source/tests/cart.test.js
@@ -17,42 +17,42 @@ test('cart screen behavior', async () => {
 
   // Simulate clicking "add-plate" button and check redirected url
   await page.click('#add-plate');
-  expect(page.url()).toBe('file://' + path.resolve(__dirname, '../plate-screen/plate-screen.html'));
+  expect((new URL(await page.url())).pathname).toBe((new URL('file://' + path.resolve(__dirname, '../plate-screen/plate-screen.html'))).pathname);
 
   // Go back to original page
   await page.goto('file://' + path.resolve(__dirname, '../cart_screen/cart.html'));
 
   // Simulate clicking "add-bowl" button and check redirected url
   await page.click('#add-bowl');
-  expect(page.url()).toBe('file://' + path.resolve(__dirname, '../bowl-screen/bowl-screen.html'));
+  expect((new URL(await page.url())).pathname).toBe((new URL('file://' + path.resolve(__dirname, '../bowl-screen/bowl-screen.html'))).pathname);
 
   // Go back to original page
   await page.goto('file://' + path.resolve(__dirname, '../cart_screen/cart.html'));
 
-  // Simulate clicking "confirm" button and check redirected url
+  // Simulate clicking "purchase" button and check redirected url
   await page.click('#confirm');
-  expect(page.url()).toBe('file://' + path.resolve(__dirname, '../cookie_screen/cookie_screen.html'));
+  expect((new URL(await page.url())).pathname).toBe((new URL('file://' + path.resolve(__dirname, '../cookie_screen/cookie_screen.html'))).pathname);
 
   // Go back to original page
   await page.goto('file://' + path.resolve(__dirname, '../cart_screen/cart.html'));
 
-  // Simulate clicking "deletebtn" button and check confirm-delete display style
+  // Simulate clicking "deleteItem" button and check confirm-delete display style
   await page.click('.deletebtn');
   let style = await page.$eval("#confirm-delete", (el) => el.style.display);
   expect(style).toBe('block');
 
   // Simulate clicking "confirmDelete" button and check confirm-delete display style
-  await page.click('.confirmdlt');  // use the class name
+  await page.click('.confirmdlt');
   style = await page.$eval("#confirm-delete", (el) => el.style.display);
   expect(style).toBe('none');
 
-  // Simulate clicking "clear" button and check confirm-clear display style
+  // Simulate clicking "popUp" button and check confirm-clear display style
   await page.click('#clear');
   style = await page.$eval("#confirm-clear", (el) => el.style.display);
   expect(style).toBe('block');
 
   // Simulate clicking "confirmClear" button, check confirm-clear display style, and items innerHTML
-  await page.click('.confirmbtn');  // use the class name
+  await page.click('.confirmbtn');
   style = await page.$eval("#confirm-clear", (el) => el.style.display);
   expect(style).toBe('none');
   const items = await page.$eval("#items", (el) => el.innerHTML);
