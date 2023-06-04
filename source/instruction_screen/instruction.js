46ddf9bd7d0dc3b7fcd9942e5f0a90530e7c515b
pthaha <pt200038@gmail.com>
2023-06-04 15:19:46 -0700

    update instructions

--- source/instruction_screen/instruction.js
+++ source/instruction_screen/instruction.js
@@ -1,28 +1,31 @@
 document.addEventListener("DOMContentLoaded", function() {
-    var instructionsList = document.getElementById("instructionsList");
-    var instructions = instructionsList.getElementsByClassName("instruction");
-    var currentIndex = 0;
+    let instructionsList = document.getElementById("instructionsList");
+    let instructions = instructionsList.getElementsByClassName("instruction");
+    let currentIndex = 0;
   
     showInstruction(currentIndex);
+    for (let i = 0; i < instructions.length; i++) {
+      console.log(instructions[i]);
+    }
   
-    var goBackButton = document.getElementById("goBackButton");
+    let goBackButton = document.getElementById("goBackButton");
     goBackButton.addEventListener("click", function() {
       window.location.href = "/group/source/opening_screen/opening-screen.html"; // Redirect to home screen
     });
   
-    var proceedButton = document.getElementById("proceedButton");
+    let proceedButton = document.getElementById("proceedButton");
     proceedButton.addEventListener("click", function() {
       window.location.href = "/group/source/cart_screen/cart.html"; // Redirect to cart screen
     });
   
-    var prevArrow = document.getElementById("prevArrow");
+    let prevArrow = document.getElementById("prevArrow");
     prevArrow.addEventListener("click", function() {
       hideInstruction(currentIndex);
       currentIndex = (currentIndex - 1 + instructions.length) % instructions.length;
       showInstruction(currentIndex);
     });
   
-    var nextArrow = document.getElementById("nextArrow");
+    let nextArrow = document.getElementById("nextArrow");
     nextArrow.addEventListener("click", function() {
       hideInstruction(currentIndex);
       currentIndex = (currentIndex + 1) % instructions.length;
