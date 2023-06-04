3315a24ca05820b2a9e19f6f7b24850e2c3305d1
Min <jaeminko0420@gmail.com>
2023-06-04 16:24:39 -0700

    update tests

--- source/tests/bowl.test.js
+++ source/tests/bowl.test.js
@@ -1,17 +1,10 @@
-const puppeteer = require('puppeteer');
-const path = require('path');
+const puppeteer = require("puppeteer");
+const path = require("path");
 
 let browser, page;
 
 beforeAll(async () => {
-  // launch browser
-  browser = await puppeteer.launch(
-    {
-      headless: false, // headless mode set to false so browser opens up with visual feedback
-      slowMo: 50 // how slow actions should be
-    }
-  );
-  // creates a new page in the opened browser 
+  browser = await puppeteer.launch();
   page = await browser.newPage();
 });
 
@@ -21,38 +14,50 @@ afterAll(async () => {
 
 describe("Bowl Screen Tests", () => {
   test("User can select a Main", async () => {
-    await page.goto('file://' + path.resolve(__dirname, '../bowl-screen/bowl-screen.html'));
+    await page.goto(
+      "file://" + path.resolve(__dirname, "../bowl-screen/bowl-screen.html")
+    );
 
     // Select the first menu-card under mains
-    await page.click('form .menu-card:nth-child(1)');
+    await page.click("form .menu-card:nth-child(1)");
 
-    const selectedImage = await page.$eval("form .menu-card:nth-child(1)", (el) => el.classList.contains('selected'));
+    const selectedImage = await page.$eval(
+      "form .menu-card:nth-child(1)",
+      (el) => el.classList.contains("selected")
+    );
     expect(selectedImage).toBe(true);
   });
 
   test("User can select an Entree", async () => {
-    await page.goto('file://' + path.resolve(__dirname, '../bowl-screen/bowl-screen.html'));
+    await page.goto(
+      "file://" + path.resolve(__dirname, "../bowl-screen/bowl-screen.html")
+    );
 
     // Select the first menu-card under entrees
-    await page.click('form .menu-card:nth-child(5)');
+    await page.click("form .menu-card:nth-child(5)");
 
-    const selectedImageEntree = await page.$eval("form .menu-card:nth-child(5)", (el) => el.classList.contains('selected'));
+    const selectedImageEntree = await page.$eval(
+      "form .menu-card:nth-child(5)",
+      (el) => el.classList.contains("selected")
+    );
     expect(selectedImageEntree).toBe(true);
   });
 
   test("User can navigate to cart", async () => {
-    await page.goto('file://' + path.resolve(__dirname, '../bowl-screen/bowl-screen.html'));
+    await page.goto(
+      "file://" + path.resolve(__dirname, "../bowl-screen/bowl-screen.html")
+    );
 
     // click the cart button
     await Promise.all([
       page.waitForNavigation(), // The promise resolves after navigation has finished
-      page.click('button'), // Clicking the link will indirectly cause a navigation
+      page.click("button"), // Clicking the link will indirectly cause a navigation
     ]);
 
     // get the current url
     const url = await page.url();
 
     // check the url contains 'cart.html'
-    expect(url).toContain('cart.html');
+    expect(url).toContain("cart.html");
   });
 });
--- source/tests/cart.test.js
+++ source/tests/cart.test.js
@@ -12,47 +12,47 @@ afterAll(() => {
   browser.close();
 });
 
-test('food screen behavior', async () => {
+test('cart screen behavior', async () => {
   await page.goto('file://' + path.resolve(__dirname, '../cart_screen/cart.html'));
-  
-  // Simulate clicking "addPlate" button and check redirected url
-  await page.click('#addPlate-button');
+
+  // Simulate clicking "add-plate" button and check redirected url
+  await page.click('#add-plate');
   expect(page.url()).toBe('file://' + path.resolve(__dirname, '../plate-screen/plate-screen.html'));
 
   // Go back to original page
   await page.goto('file://' + path.resolve(__dirname, '../cart_screen/cart.html'));
 
-  // Simulate clicking "addBowl" button and check redirected url
-  await page.click('#addBowl-button');
+  // Simulate clicking "add-bowl" button and check redirected url
+  await page.click('#add-bowl');
   expect(page.url()).toBe('file://' + path.resolve(__dirname, '../bowl-screen/bowl-screen.html'));
 
   // Go back to original page
   await page.goto('file://' + path.resolve(__dirname, '../cart_screen/cart.html'));
 
-  // Simulate clicking "purchase" button and check redirected url
-  await page.click('#purchase-button');
+  // Simulate clicking "confirm" button and check redirected url
+  await page.click('#confirm');
   expect(page.url()).toBe('file://' + path.resolve(__dirname, '../cookie_screen/cookie_screen.html'));
 
   // Go back to original page
   await page.goto('file://' + path.resolve(__dirname, '../cart_screen/cart.html'));
 
-  // Simulate clicking "deleteItem" button and check confirm-delete display style
-  await page.click('#deleteItem-button');
+  // Simulate clicking "deletebtn" button and check confirm-delete display style
+  await page.click('.deletebtn');
   let style = await page.$eval("#confirm-delete", (el) => el.style.display);
   expect(style).toBe('block');
 
   // Simulate clicking "confirmDelete" button and check confirm-delete display style
-  await page.click('#confirmDelete-button');
+  await page.click('.confirmdlt');  // use the class name
   style = await page.$eval("#confirm-delete", (el) => el.style.display);
   expect(style).toBe('none');
 
-  // Simulate clicking "popUp" button and check confirm-clear display style
-  await page.click('#popUp-button');
+  // Simulate clicking "clear" button and check confirm-clear display style
+  await page.click('#clear');
   style = await page.$eval("#confirm-clear", (el) => el.style.display);
   expect(style).toBe('block');
 
   // Simulate clicking "confirmClear" button, check confirm-clear display style, and items innerHTML
-  await page.click('#confirmClear-button');
+  await page.click('.confirmbtn');  // use the class name
   style = await page.$eval("#confirm-clear", (el) => el.style.display);
   expect(style).toBe('none');
   const items = await page.$eval("#items", (el) => el.innerHTML);
