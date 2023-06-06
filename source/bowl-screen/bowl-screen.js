5cc35e394fa014da45310d885c04d3719b322e00
P-Z-H <z7peng@ucsd.edu>
2023-06-06 12:22:27 -0700

    update linting

--- source/bowl-screen/bowl-screen.js
+++ source/bowl-screen/bowl-screen.js
@@ -1,27 +1,52 @@
-let selectedCount = 0
-let selectedCountEntree = 0
+let selectedCount = 0;
+let selectedCountEntree = 0;
 
-function selectImage (element) {
+function selectImage(element) {
   if (element.classList.contains('selected')) {
-    element.classList.remove('selected')
-    selectedCount--
+    element.classList.remove('selected');
+    selectedCount--;
   } else if (selectedCount < 1) {
-    element.classList.add('selected')
-    selectedCount++
+    element.classList.add('selected');
+    selectedCount++;
   }
 }
 
-function selectImageEntree (element) {
-  if (element.classList.contains('selected')) {
-    element.classList.remove('selected')
-    selectedCountEntree--
+function selectImageEntree(element) {
+  if (element.classList.contains('selectedEntree')) {
+    element.classList.remove('selectedEntree');
+    selectedCountEntree--;
   } else if (selectedCountEntree < 1) {
-    element.classList.add('selected')
-    selectedCountEntree++
+    element.classList.add('selectedEntree');
+    selectedCountEntree++;
   }
 }
 
-function goToCart () {
-  location.href = '/source/cart_screen/cart.html'
-  console.log('Function Called!')
+function goToCart() {
+  location.href = "/source/cart_screen/cart.html";
+  console.log("Function Called!");
+}
+
+function saveSelectedItems() {
+  const selectedItems = {
+    main: getSelectedItems('.menu-card.selected'),
+    entree: getSelectedItems('.menu-card.selectedEntree')
+  };
+
+  // Store selected items in LocalStorage as JSON
+  localStorage.setItem('selectedItems', JSON.stringify(selectedItems));
+}
+
+function getSelectedItems(selector) {
+  const selectedItems = [];
+
+  const selectedElements = document.querySelectorAll(selector);
+  selectedElements.forEach((element) => {
+    const itemDetails = {
+      name: element.querySelector('h3').innerText,
+      price: element.querySelector('p:nth-child(3)').innerText
+    };
+    selectedItems.push(itemDetails);
+  });
+
+  return selectedItems;
 }
--- source/instruction_screen/instruction.js
+++ source/instruction_screen/instruction.js
@@ -1,41 +1,31 @@
-document.addEventListener('DOMContentLoaded', function () {
-  const instructionsList = document.getElementById('instructionsList')
-  const instructions = instructionsList.getElementsByClassName('instruction')
-  let currentIndex = 0
-
-  showInstruction(currentIndex)
-
-  const prevArrow = document.getElementById('prevArrow')
-  prevArrow.addEventListener('click', function () {
-    hideInstruction(currentIndex)
-    currentIndex = (currentIndex - 1 + instructions.length) % instructions.length
-    showInstruction(currentIndex)
-  })
-
-  const nextArrow = document.getElementById('nextArrow')
-  nextArrow.addEventListener('click', function () {
-    hideInstruction(currentIndex)
-    currentIndex = (currentIndex + 1) % instructions.length
-    showInstruction(currentIndex)
-  })
-
-  function showInstruction (index) {
-    if (index >= 0 && index < instructions.length) {
-      instructions[index].style.display = 'block'
-    }
-  }
-
-  function hideInstruction (index) {
-    if (index >= 0 && index < instructions.length) {
-      instructions[index].style.display = 'none'
-    }
-  }
-})
-
-function play () {
-  window.location.href = '/source/cart_screen/cart.html'
+function goBack() {
+  window.location.href = "/source/opening_screen/opening-screen.html";
 }
 
-function goBack () {
-  window.location.href = '/source/opening_screen/opening-screen.html'
-}
+const keyframesAnimation = `
+    @keyframes fade {
+        from {
+            opacity: 0;
+        }
+        to {
+            opacity: 1;
+        }
+    }
+
+    @keyframes glossyAnimation {
+        0% {
+            background-position: 0% 50%;
+        }
+        50% {
+            background-position: 100% 50%;
+        }
+        100% {
+            background-position: 0% 50%;
+        }
+    }
+`;
+
+const styleElement = document.createElement('style');
+styleElement.type = 'text/css';
+styleElement.innerHTML = keyframesAnimation;
+document.head.appendChild(styleElement);
