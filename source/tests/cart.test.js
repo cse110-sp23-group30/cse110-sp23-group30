543e8be24a8eb49d736afe43b9beab246522033a
Min <jaeminko0420@gmail.com>
2023-06-06 16:53:14 -0700

    Update cart.test.js

--- source/tests/cart.test.js
+++ source/tests/cart.test.js
@@ -19,24 +19,30 @@ describe('Cart Page', () => {
   });
 
   test('Add Plate button redirects to the correct page', async () => {
-    await page.click('#add-plate');
-    expect(await page.url()).toContain('/source/plate-screen/plate-screen.html'); // adjust as necessary
+    await Promise.all([
+      page.click('#add-plate'),
+      page.waitForNavigation({ waitUntil: 'networkidle0' }),
+    ]);
+    expect(await page.url()).toContain('/source/plate-screen/plate-screen.html');
   });
 
   test('Add Bowl button redirects to the correct page', async () => {
     await page.goBack();
+    await page.waitForSelector('#add-bowl');
     await page.click('#add-bowl');
     expect(await page.url()).toContain('/source/bowl-screen/bowl-screen.html'); // adjust as necessary
   });
 
   test('Confirm Purchase button redirects to the correct page', async () => {
     await page.goBack();
+    await page.waitForSelector('#confirm');
     await page.click('#confirm');
     expect(await page.url()).toContain('/source/cookie_screen/cookie_screen.html'); // adjust as necessary
   });
 
   test('Clear Cart button pops up the confirm-clear element', async () => {
     await page.goBack();
+    await page.waitForSelector('#clear');
     await page.click('#clear');
     const displayStyle = await page.$eval('#confirm-clear', el => getComputedStyle(el).display);
     expect(displayStyle).toBe('block');
@@ -44,6 +50,7 @@ describe('Cart Page', () => {
 
   test('Delete button pops up the confirm-delete element', async () => {
     // This test assumes that there is at least one delete button (class = "deletebtn") on the page
+    await page.waitForSelector('.deletebtn');
     await page.click('.deletebtn');
     const displayStyle = await page.$eval('#confirm-delete', el => getComputedStyle(el).display);
     expect(displayStyle).toBe('block');
