2b9a9d70c54f22f8d461b454e54eedcc8dca32c2
Eric Dong <e1dong@ucsd.edu>
2023-06-06 01:02:14 -0700

    updates

--- source/header/header-component.js
+++ source/header/header-component.js
@@ -1,9 +1,9 @@
 class HeaderComponent extends HTMLElement {
-    connectedCallback() {
-      this.innerHTML = `
+  connectedCallback() {
+    this.innerHTML = `
         <header>
           <div class="container">
-            <img src="../assets/BlueMoon.png" alt="Logo" class="logo">
+            <img src="source/assets/BlueMoon.png" alt="Logo" class="logo">
             <h1 class="header-title">The Blue Moon Express</h1>
             <div class="button-group">
               <button id="homeBtn">Home</button>
@@ -13,48 +13,46 @@ class HeaderComponent extends HTMLElement {
           </div>
         </header>
       `;
-  
-      // Add event listeners for the buttons
-      this.querySelector('#homeBtn').addEventListener('click', () => {
-        // Handle home button click
-        window.location.href = '../opening_screen/opening-screen.html';
+
+    // Add event listeners for the buttons
+    this.querySelector("#homeBtn").addEventListener("click", () => {
+      // Handle home button click
+      window.location.href = "source/opening_screen/opening-screen.html";
+    });
+
+    this.querySelector("#instructionsBtn").addEventListener("click", () => {
+      // Handle instructions button click
+      window.location.href = "source/instruction_screen/instruction.html";
+    });
+
+    // Get a reference to the settings button
+    const settingsBtn = this.querySelector("#settingsBtn");
+
+    // Add event listener to the settings button
+    settingsBtn.addEventListener("click", () => {
+      // Check if the settings panel already exists
+      document.getElementById("settings-popup").style.display = "block";
+
+      const volumeSlider = document.getElementById("musicVolume");
+      volumeSlider.addEventListener("input", () => {
+        // Get a reference to the audio element
+        const audioPlayer = document.getElementById("audioPlayer");
+        // Adjust the music volume based on the slider value
+        const musicVolume = volumeSlider.value / 100;
+        audioPlayer.volume = musicVolume;
+        audioPlayer.play();
       });
-  
-      this.querySelector('#instructionsBtn').addEventListener('click', () => {
-        // Handle instructions button click
-        window.location.href = '../instruction_screen/instruction.html';
-      });
-      
-      
-      // Get a reference to the settings button
-      const settingsBtn = this.querySelector('#settingsBtn');
-      
-      // Add event listener to the settings button
-      settingsBtn.addEventListener('click', () => {
-       // Check if the settings panel already exists
-        document.getElementById("settings-popup").style.display = "block";
+    });
 
-        const volumeSlider = document.getElementById("musicVolume");
-        volumeSlider.addEventListener('input', () => {
-          // Get a reference to the audio element
-          const audioPlayer = document.getElementById('audioPlayer');
-          // Adjust the music volume based on the slider value
-          const musicVolume = volumeSlider.value / 100;
-          audioPlayer.volume = musicVolume;
-          audioPlayer.play();
-        });
-      });
+    const closeSettings = document.getElementsByClassName("close")[0];
 
-      const closeSettings = document.getElementsByClassName("close")[0]
-
-      closeSettings.addEventListener('click', () => {
-        document.getElementById("settings-popup").style.display = "none";
-      })
-
-    }
+    closeSettings.addEventListener("click", () => {
+      document.getElementById("settings-popup").style.display = "none";
+    });
   }
-  
-  customElements.define('header-component', HeaderComponent);
+}
+
+customElements.define("header-component", HeaderComponent);
 
 // // Dynamically insert the header into each page
 // window.addEventListener('DOMContentLoaded', () => {
@@ -62,4 +60,3 @@ class HeaderComponent extends HTMLElement {
 //   const headerComponent = document.createElement('header-component');
 //   headerContainer.appendChild(headerComponent);
 // });
-  
