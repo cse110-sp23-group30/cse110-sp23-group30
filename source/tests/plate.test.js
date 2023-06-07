3c914ab696edc8d7de978b4947b5c1e8b37df5da
P-Z-H <z7peng@ucsd.edu>
2023-06-07 16:43:50 -0700

    fix plate

--- source/tests/plate.test.js
+++ source/tests/plate.test.js
@@ -20,11 +20,11 @@ describe('selectImage', () => {
   const mockElement = document.createElement('div')
   it('should add "selected" class if not already present and selectedCount < 2', () => {
     functions.selectImage(mockElement)
-    expect(mockElement.classList[0] == 'selected').toBe(true)
+    expect(mockElement.classList[0] == 'selectedEntree').toBe(true)
   })
 
   it('should remove "selected" class if present', () => {
     functions.selectImage(mockElement)
-    expect(mockElement.classList[0] == 'selected').toBe(false)
+    expect(mockElement.classList[0] == 'selectedEntree').toBe(false)
   })
 })
