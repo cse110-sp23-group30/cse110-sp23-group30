1a9a354b354753010b11619e878a528ae942ed9b
P-Z-H <z7peng@ucsd.edu>
2023-06-07 11:49:08 -0700

    update plate

---
+++ source/tests/plate.test.js
@@ -0,0 +1,53 @@
+const functions = require('../plate-screen/plate-screen.js')
+
+describe('selectImageMain', () => {
+  it('should add "selected" class if not already present', () => {
+    const mockElement = {
+      classList: {
+        contains: jest.fn().mockReturnValue(false),
+        add: jest.fn(),
+        remove: jest.fn()
+      }
+    }
+    functions.selectImageMain(mockElement)
+    expect(mockElement.classList.add).toHaveBeenCalledWith('selected')
+  })
+
+  it('should remove "selected" class if present', () => {
+    const mockElement = {
+      classList: {
+        contains: jest.fn().mockReturnValue(true),
+        add: jest.fn(),
+        remove: jest.fn()
+      }
+    }
+    functions.selectImageMain(mockElement)
+    expect(mockElement.classList.remove).toHaveBeenCalledWith('selected')
+  })
+})
+
+describe('selectImage', () => {
+  it('should add "selected" class if not already present and selectedCount < 2', () => {
+    const mockElement = {
+      classList: {
+        contains: jest.fn().mockReturnValue(false),
+        add: jest.fn(),
+        remove: jest.fn()
+      }
+    }
+    functions.selectImage(mockElement)
+    expect(mockElement.classList.add).toHaveBeenCalledWith('selected')
+  })
+
+  it('should remove "selected" class if present', () => {
+    const mockElement = {
+      classList: {
+        contains: jest.fn().mockReturnValue(true),
+        add: jest.fn(),
+        remove: jest.fn()
+      }
+    }
+    functions.selectImage(mockElement)
+    expect(mockElement.classList.remove).toHaveBeenCalledWith('selected')
+  })
+})
