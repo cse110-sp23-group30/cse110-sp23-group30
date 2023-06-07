713e34833be12565a4f939a05f2c573d3d446254
Richard Xu <richardcanxu@gmail.com>
2023-06-07 14:16:22 -0700

    Changed the plate screen so it would work

--- source/plate-screen/plate-screen.js
+++ source/plate-screen/plate-screen.js
@@ -1,22 +1,67 @@
 let selectedCount = 0
-let selectedCountMain = 0
-
-function selectImageMain (element) {
-  if (element.classList.contains('selected')) {
-    element.classList.remove('selected')
-    selectedCountMain--
-  } else if (selectedCountMain < 1) {
-    element.classList.add('selected')
-    selectedCountMain++
-  }
-}
+let selectedCountEntree = 0
 
 function selectImage (element) {
   if (element.classList.contains('selected')) {
     element.classList.remove('selected')
     selectedCount--
-  } else if (selectedCount < 2) {
+  } else if (selectedCount < 1) {
     element.classList.add('selected')
     selectedCount++
   }
+}
+
+function selectImageEntree (element) {
+  if (element.classList.contains('selectedEntree')) {
+    element.classList.remove('selectedEntree')
+    selectedCountEntree--
+  } else if (selectedCountEntree < 2) {
+    element.classList.add('selectedEntree')
+    selectedCountEntree++
+  }
+}
+
+function goToCart () {
+  location.href = '/source/cart_screen/cart.html'
+  console.log('Function Called!')
+}
+
+function saveSelectedItems () {
+  // Create a either a new list or a list with everything in local storage
+  let dishes
+  if (localStorage.getItem('dishes') === null) {
+    dishes = []
+  } else {
+    dishes = JSON.parse(localStorage.getItem('dishes'))
+  }
+
+  const selectedItems = {
+    main: getSelectedItems('.menu-card.selected'),
+    entree: getSelectedItems('.menu-card.selectedEntree')
+  }
+
+  // Add selectedItems to list
+  dishes.push(selectedItems)
+
+  // Store list in LocalStorage as JSON
+  localStorage.setItem('dishes', JSON.stringify(dishes))
+
+  // Store selected items in LocalStorage as JSON
+  // localStorage.setItem("selectedItems", JSON.stringify(selectedItems));
+}
+
+function getSelectedItems (selector) {
+  const selectedItems = []
+
+  const selectedElements = document.querySelectorAll(selector)
+  selectedElements.forEach((element) => {
+    const itemDetails = {
+      src: element.querySelector('img').getAttribute('src'),
+      name: element.querySelector('h3').innerText,
+      price: element.querySelector('p:nth-child(3)').innerText
+    }
+    selectedItems.push(itemDetails)
+  })
+
+  return selectedItems
 }
