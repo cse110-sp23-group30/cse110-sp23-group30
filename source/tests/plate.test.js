23d9c11055d6926a4d5629b9a0dc0c9d38ba2e55
RichardCanXu <richardcanxu@gmail.com>
2023-06-10 15:38:56 -0700

    got rid of plate 2

--- source/tests/plate.test.js
+++ source/tests/plate.test.js
@@ -1,58 +1,35 @@
-describe("Plate Screen Tests", () => {
-    beforeAll(async () => {
-      await page.goto('http://localhost:4444/source/plate-screen/plate-screen.html');
-    });
-    test("User can select a Main", async () => {
-      
-  
-      // Select the first menu-card under mains
-      await page.click("form .menu-card:nth-child(1)");
-  
-      const selectedImage = await page.$eval(
-        "form .menu-card:nth-child(1)",
-        (el) => el.classList.contains("selected")
-      );
-      expect(selectedImage).toBe(true);
-    });
-  
-    test("User can select an Entree", async () => {
-   
-      // Select the first menu-card under entrees
-      await page.click("form .menu-card:nth-child(5)");
-  
-      const selectedImageEntree = await page.$eval(
-        "form .menu-card:nth-child(5)",
-        (el) => el.classList.contains("selected")
-      );
-      expect(selectedImageEntree).toBe(true);
-    });
+const puppeteer = require('puppeteer');
 
-    test("User can select two Entree", async () => {
-   
-        // Select the first menu-card under entrees
-        await page.click("form .menu-card:nth-child(5)");
-    
-        const selectedCount = await page.$eval(
-          "form .menu-card:nth-child(5)",
-          (el) => el.classList.contains("selected")
-        );
-        expect(selectedCount).toBe(2);
-      });
-  
-    test("User can navigate to cart", async () => {
-  
-  
-      // click the cart button
-      await Promise.all([
-        page.waitForNavigation(), // The promise resolves after navigation has finished
-        page.click("button"), // Clicking the link will indirectly cause a navigation
-      ]);
-  
-      // get the current url
-      const url = await page.url();
-  
-      // check the url contains 'cart.html'
-      expect(url).toContain("cart.html");
-    });
+describe('Plate Screen functions', () => {
+  let browser;
+  let page;
+
+  beforeAll(async () => {
+    // Launch browser
+    browser = await puppeteer.launch();
+    // Create new page
+    page = await browser.newPage();
+    // Navigate to the plate screen
+    await page.goto('http://localhost:4444/source/plate-screen/plate-screen.html');
   });
-  
+
+  afterAll(async () => {
+    // Close browser
+    await browser.close();
+  });
+
+  //test if main food image can be selected
+  test('selectImageMain', async () => {
+    await page.evaluate(() => selectImageMain(document.querySelector('.menu-card')));
+    const isSelected = await page.evaluate(() => document.querySelector('.menu-card').classList.contains('selected'));
+    expect(isSelected).toBe(true);
+  });
+
+  //test if entree food image can be selected
+  test('selectImage', async () => {
+    await page.evaluate(() => selectImage(document.querySelector('.menu-card')));
+    const isSelectedEntree = await page.evaluate(() => document.querySelector('.menu-card').classList.contains('selectedEntree'));
+    expect(isSelectedEntree).toBe(true);
+  });
+
+});
