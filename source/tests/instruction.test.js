840fc14c01de35432297beb4db268d9bca2ef7b1
Eric Dong <e1dong@ucsd.edu>
2023-06-10 03:00:24 -0700

    headless warning

--- source/tests/instruction.test.js
+++ source/tests/instruction.test.js
@@ -7,7 +7,7 @@ let browser;
 let page;
 
 beforeAll(async () => {
-  browser = await puppeteer.launch();
+  browser = await puppeteer.launch({ headless: 'new' });
   page = await browser.newPage();
 });
 
